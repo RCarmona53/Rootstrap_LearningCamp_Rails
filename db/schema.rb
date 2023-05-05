aa9cccf6eab1b9a7d90fbd2796c2616e1ef3a4ec
Rodrigo <carrmona5345@icloud.com>
2023-05-05 09:00:58 -0300

    Blog Project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_143554) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_171244) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
@@ -19,4 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_143554) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
